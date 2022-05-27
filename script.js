4174f60e9258b647fc4a1060ea982210468e3f94
CoffeeIsTrash <public.davidbuday@gmail.com>
2022-05-27 09:31:07 +0200

    Implemented better handling for numbers and decimal

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
 
 // Getting all elements and mapping event listeners
 const screen = document.querySelector("#result");
+
 [...document.querySelectorAll(".number")].map(number => {
-    number.addEventListener('click', handleNumberClick, false);
+    number.addEventListener('click', (e)=>{
+        if(e.target.innerHTML === '.' && !haveDot) haveDot = true;
+        else if(e.target.innerHTML === '.' && haveDot) return;
+        currentValue += e.target.innerText;
+        screen.value = currentValue;
+    });
 });
+
 [...document.querySelectorAll(".operator")].map(operation => {
     operation.addEventListener('click', handleOperation, false);
 });
 document.querySelector(".equal").addEventListener('click', handleCalculation);
-document.querySelector('.decimal').addEventListener('click', handleDecimal);
 document.querySelector('.ac').addEventListener('click', ac);
 
 // Creating basic variables for storing data
@@ -17,11 +23,9 @@ let operator = null;
 let currentValue = "";
 let prevValue = "";
 
+let haveDot = false;
+
 // Functionality and handling of events
-function handleNumberClick(){
-    currentValue += this.innerHTML;
-    screen.value = currentValue;
-}
 function handleOperation(){
     operator = this.innerHTML;
     prevValue = currentValue;
@@ -32,10 +36,6 @@ function handleCalculation(){
     currentValue = calculate(prevValue, operator, currentValue).toFixed(3);
     screen.value = currentValue;
     prevValue = currentValue;
-}
-function handleDecimal(){
-    currentValue += ".";
-    screen.value = currentValue;
 }
 // Basic calculate function
 function calculate(v1, operator, v2){
